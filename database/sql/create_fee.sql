21c72c8f0823260ffd8a5bcffd10815834213e4b
Frank Lanitz <frank@frank.uvena.de>
2012-03-16 13:44:54 +0100

    Added table for fee per hacker

---
+++ database/sql/create_fee.sql
@@ -0,0 +1,34 @@
+DROP TABLE fee;
+
+CREATE TABLE fee (
+	id integer NOT NULL,
+	hacker integer, 
+	fee_group integer,
+	extra_fee money,
+	valid_from date,
+	valid_to date,
+	deleted boolean,
+    created_ts date,
+    updated_ts date,
+    deleted_ts date
+);
+	
+ALTER TABLE public.fee OWNER TO postgres;
+
+CREATE SEQUENCE fee_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER TABLE public.fee_id_seq OWNER TO postgres;
+
+ALTER SEQUENCE fee_id_seq OWNED BY fee.id;
+
+SELECT pg_catalog.setval('fee_id_seq', 1, true);
+
+ALTER TABLE ONLY fee ALTER COLUMN id SET DEFAULT nextval('fee_id_seq'::regclass);
+
+ALTER TABLE ONLY fee
+    ADD CONSTRAINT fee_id PRIMARY KEY (id);
