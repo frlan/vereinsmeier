d1580f83542bb6fe474e169c553e6327eb4969fa
Frank Lanitz <frank@frank.uvena.de>
2012-04-06 12:03:01 +0200

    Use implizit sequenz on hacker

--- database/sql/create_hacker.sql
+++ database/sql/create_hacker.sql
@@ -1,7 +1,7 @@
 DROP TABLE hacker;
 
 CREATE TABLE hacker (
-    id integer NOT NULL,
+    id serial NOT NULL,
     member_since date, 
     member_till date CHECK(member_till >= member_since),
     deleted boolean,
@@ -11,19 +11,6 @@ CREATE TABLE hacker (
 );
 
 ALTER TABLE public.hacker OWNER TO postgres;
-
-CREATE SEQUENCE hacker_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-ALTER TABLE public.hacker_id_seq OWNER TO postgres;
-
-ALTER SEQUENCE hacker_id_seq OWNED BY hacker.id;
-
-ALTER TABLE ONLY hacker ALTER COLUMN id SET DEFAULT nextval('hacker_id_seq'::regclass);
 
 ALTER TABLE ONLY hacker
     ADD CONSTRAINT hacker_id PRIMARY KEY (id);
