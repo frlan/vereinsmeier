1ee40ce7a9a8c3a40177e61585214cabe8d55d51
Frank Lanitz <frank@frank.uvena.de>
2012-04-06 12:01:54 +0200

    Using implicit sequenz on fee_groups too

--- database/sql/create_fee_groups.sql
+++ database/sql/create_fee_groups.sql
@@ -1,7 +1,7 @@
 DROP TABLE fee_groups;
 
 CREATE TABLE fee_groups (
-    id integer NOT NULL,
+    id serial NOT NULL,
     role_name character varying(50),
     fee money CHECK (fee >= 0::money),
     valid_from date,
@@ -13,26 +13,6 @@ CREATE TABLE fee_groups (
 );
 
 ALTER TABLE public.fee_groups OWNER TO postgres;
-
---
--- Name: fee_groups_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE fee_groups_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.fee_groups_id_seq OWNER TO postgres;
-
---
--- Name: fee_groups_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE fee_groups_id_seq OWNED BY fee_groups.id;
 
 ALTER TABLE ONLY fee_groups
     ADD CONSTRAINT fee_groups_id PRIMARY KEY (id);
