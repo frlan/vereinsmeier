5590b3e046400cf9c91d0251e7476adf05ddfc12
Frank Lanitz <frank@frank.uvena.de>
2012-04-06 10:46:47 +0200

    Added a script to create database

---
+++ database/sql/create_database.sql
@@ -0,0 +1,3 @@
+DROP DATABASE vereinsmeier;
+
+CREATE DATABASE vereinsmeier;
--- database/sql/init_database.sh
+++ database/sql/init_database.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+psql -h 127.0.0.1 -U postgres -f create_database.sql
 psql -h 127.0.0.1 -U postgres vereinsmeier -f create_hacker.sql
 psql -h 127.0.0.1 -U postgres vereinsmeier -f create_fee_groups.sql
 psql -h 127.0.0.1 -U postgres vereinsmeier -f create_fee.sql
