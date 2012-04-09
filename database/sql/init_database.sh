d1c75878f863b70cc25baaa6c5d0055a077c7cd6
Frank Lanitz <frank@frank.uvena.de>
2012-04-09 18:14:45 +0200

    Adding PL/Python to database

--- database/sql/init_database.sh
+++ database/sql/init_database.sh
@@ -7,3 +7,6 @@ psql -h 127.0.0.1 -U postgres vereinsmeier -f create_fee.sql
 psql -h 127.0.0.1 -U postgres vereinsmeier -f create_entry.sql
 psql -h 127.0.0.1 -U postgres vereinsmeier -f create_hacker_entry.sql
 psql -h 127.0.0.1 -U postgres vereinsmeier -f create_hacker_data.sql
+
+# Adding PL/Python to database
+createlang -U postgres -h 127.0.0.1 plpythonu vereinsmeier
