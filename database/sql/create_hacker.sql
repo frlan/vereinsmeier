097c3357b3d59279e372f9fce29efd82458dae3b
Frank Lanitz <frank@frank.uvena.de>
2012-04-09 20:44:17 +0200

    Ensure some fileds are != NULL

--- database/sql/create_hacker.sql
+++ database/sql/create_hacker.sql
@@ -2,7 +2,7 @@ DROP TABLE hacker;
 
 CREATE TABLE hacker (
     id serial PRIMARY KEY,
-    member_since date, 
+    member_since date NOT NULL, 
     member_till date CHECK(member_till >= member_since),
     deleted boolean,
     created_ts date,
