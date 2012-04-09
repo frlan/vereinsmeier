ded0404dfa326ac7779919e867cd038948121291
Frank Lanitz <frank@frank.uvena.de>
2012-04-09 20:28:48 +0200

    Name of a fee_group shall not be empty

--- database/sql/create_fee_groups.sql
+++ database/sql/create_fee_groups.sql
@@ -2,7 +2,7 @@ DROP TABLE fee_groups;
 
 CREATE TABLE fee_groups (
     id serial PRIMARY KEY,
-    role_name character varying(50),
+    role_name character varying(50) NOT NULL,
     fee money CHECK (fee >= 0::money),
     valid_from date,
     valid_to date,
