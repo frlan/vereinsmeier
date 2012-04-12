6a26848004755bf0b8447c171efb387e4a18d6e8
Frank Lanitz <frank@frank.uvena.de>
2012-04-12 16:44:23 +0200

    Move init script to bin folder

---
+++ database/bin/init_database.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+psql -h 127.0.0.1 -U postgres -f ../sql/create_database.sql
+psql -h 127.0.0.1 -U postgres vereinsmeier -f ../sql/create_hacker.sql
+psql -h 127.0.0.1 -U postgres vereinsmeier -f ../sql/create_fee_groups.sql
+psql -h 127.0.0.1 -U postgres vereinsmeier -f ../sql/create_fee.sql
+psql -h 127.0.0.1 -U postgres vereinsmeier -f ../sql/create_entry.sql
+psql -h 127.0.0.1 -U postgres vereinsmeier -f ../sql/create_hacker_entry.sql
+psql -h 127.0.0.1 -U postgres vereinsmeier -f ../sql/create_hacker_data.sql
+psql -h 127.0.0.1 -U postgres vereinsmeier -f ../sql/create_account_status.sql
+
+# Adding PL/Python to database
+createlang -U postgres -h 127.0.0.1 plpythonu vereinsmeier
