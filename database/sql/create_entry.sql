f2785cd321998868b245e1c239684596f66a9bb6
Frank Lanitz <frank@frank.uvena.de>
2012-04-12 16:44:45 +0200

    Tab vs. Space

--- database/sql/create_entry.sql
+++ database/sql/create_entry.sql
@@ -1,13 +1,13 @@
 DROP TABLE entry;
 
 CREATE TABLE entry (
-	id SERIAL PRIMARY KEY,
-	value money,
-	ref1 varchar(255),
-	ref2 varchar(255),
-	entry_date date,
-	comment varchar(255),
-	deleted boolean DEFAULT false,
+    id SERIAL PRIMARY KEY,
+    value money,
+    ref1 varchar(255),
+    ref2 varchar(255),
+    entry_date date,
+    comment varchar(255),
+    deleted boolean DEFAULT false,
     created_ts date,
     updated_ts date,
     deleted_ts date
