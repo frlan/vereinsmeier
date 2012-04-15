9729ad0652ed1a16f575df813612d7bdd7d3d049
Frank Lanitz <frank@frank.uvena.de>
2012-04-15 15:36:47 +0200

    Fixing init_database script

--- database/bin/init_database.sh
+++ database/bin/init_database.sh
@@ -4,7 +4,7 @@ psql -h 127.0.0.1 -U postgres -f ../sql/create_database.sql
 psql -h 127.0.0.1 -U postgres vereinsmeier -f ../sql/create_hacker.sql
 psql -h 127.0.0.1 -U postgres vereinsmeier -f ../sql/create_fee_groups.sql
 psql -h 127.0.0.1 -U postgres vereinsmeier -f ../sql/create_fee.sql
-psql -h 127.0.0.1 -U postgres vereinsmeier -f ../sql/create_entry.sql
+psql -h 127.0.0.1 -U postgres vereinsmeier -f ../sql/create_entries.sql
 psql -h 127.0.0.1 -U postgres vereinsmeier -f ../sql/create_hacker_entry.sql
 psql -h 127.0.0.1 -U postgres vereinsmeier -f ../sql/create_hacker_data.sql
 psql -h 127.0.0.1 -U postgres vereinsmeier -f ../sql/create_account_status.sql
