620f6a1b0d9104e7f741f402f9a4458aa2cde23e
Frank Lanitz <frank@frank.uvena.de>
2012-04-19 17:43:56 +0200

    Adding two more status (deleted, create_ts...) to tables

--- database/sql/create_entry_typ.sql
+++ database/sql/create_entry_typ.sql
@@ -1,5 +1,9 @@
 CREATE TABLE entry_type(
     id serial PRIMARY KEY,
     entry integer REFERENCES entries,
-    typ integer REFERENCES entry_types
+    typ integer REFERENCES entry_types,
+    deleted boolean DEFAULT false,
+    created_ts timestamp DEFAULT now(),
+    updated_ts timestamp,
+    deleted_ts timestamp
 );
--- database/sql/create_entry_types.sql
+++ database/sql/create_entry_types.sql
@@ -1,4 +1,8 @@
 CREATE TABLE entry_types(
     id serial PRIMARY KEY,
-    name character varying(100)
+    name character varying(100),
+    deleted boolean DEFAULT false,
+    created_ts timestamp DEFAULT now(),
+    updated_ts timestamp,
+    deleted_ts timestamp
 );
