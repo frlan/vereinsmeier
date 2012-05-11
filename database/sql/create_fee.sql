b025bf22a5dc4a489c9ae870d60c02d07f5f13e2
Frank Lanitz <frank@frank.uvena.de>
2012-05-11 17:03:51 +0200

    Setting default to now on valid_from at fee

--- database/sql/create_fee.sql
+++ database/sql/create_fee.sql
@@ -5,7 +5,7 @@ CREATE TABLE fee (
     hacker integer REFERENCES hacker ON DELETE CASCADE,
     fee_group integer REFERENCES fee_groups ON DELETE SET NULL,
     extra_fee money DEFAULT 0 CHECK (extra_fee >= 0::money),
-    valid_from date NOT NULL,
+    valid_from date NOT NULL DEFAULT now(),
     valid_to date,
     deleted boolean DEFAULT false,
     created_ts timestamp DEFAULT now(),
