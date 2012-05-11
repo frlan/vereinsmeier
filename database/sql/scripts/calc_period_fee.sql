3068db185ad60628926a52da91368b1aa201c8a7
Frank Lanitz <frank@frank.uvena.de>
2012-05-11 18:00:45 +0200

    Also include extra_fee into script

--- database/sql/scripts/calc_period_fee.sql
+++ database/sql/scripts/calc_period_fee.sql
@@ -1,7 +1,13 @@
 -- A script to generate a list of monthly amount to be paid by 
 -- a member on period starting with date used inside query
+-- Replace the date by your cutt-off day of the period
 
-SELECT hacker, '2012-05-01', fee_groups.fee, 'Verrechnung Monatsbeitrag', 'Generiert' 
+SELECT 
+	hacker as hackerid, 
+	'2012-05-01'::date as "cutt-off-day", 
+	fee_groups.fee + fee.extra_fee as amount, 
+	'Verrechnung Monatsbeitrag' as description, 
+	'Generiert' as comment
 FROM fee, fee_groups 
 WHERE 
 fee.fee_group = fee_groups.id
